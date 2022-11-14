c5d25c20842f1ef7b28336a6891b87665179d375
AswathiMohan23 <achu999here@gmail.com>
2022-11-14 15:52:32 +0530

    [ADDED] : code to remove the added mail id from the list

--- src/com/java/UserRegistration.java
+++ src/com/java/UserRegistration.java
@@ -12,40 +12,58 @@ public class UserRegistration {
         isEmailValid();
         isValidPhoneNumber();
         isValidPassword();
-
+        removingTheAddedMailId();
     }
 
+    private static void removingTheAddedMailId() {
+        System.out.println("\n========================================== UC9 ===============================================\n");
+        System.out.println("removing email : ");
+        System.out.println("list after removing the added mail id : "+list);
+    }
+
+
     private static void isValidFirstName() {
-        System.out.println("\n\n========================================== UC1 ===============================================\n");
+        System.out.println("\n========================================== UC1 ===============================================\n");
         boolean result = nameValidation("Harry");
-        System.out.println("\nthe first name is =====>>>>>  "+result);
+        System.out.println("the first name is =====>>>>>  "+result);
     }
     private static void isValidSecondName() {
         System.out.println("\n\n========================================== UC2 ===============================================\n");
         boolean result = nameValidation("Potter");
-        System.out.println("\nthe second name is =====>>>>>  "+result);
+        System.out.println("the second name is =====>>>>>  "+result);
     }
     private static boolean nameValidation(String name){
         boolean result=false;
         String regex="[A-Z]{1}[a-z]{3,}";// {3,} means above 3
         Pattern pattern=Pattern.compile(regex);
-        for(int i=0;i<name.length();i++){
-            Matcher matcher=pattern.matcher(name);
-            result=matcher.matches();
-        }
+        Matcher matcher=pattern.matcher(name);
+        result=matcher.matches();
+        if(result==true)
+            list.add(name);
+        else
+            list.add("-------------");
         return result;
     }
     private static void isEmailValid(){
         System.out.println("\n\n========================================== UC3 ===============================================\n");
-        boolean result = false;
-        String email="abcd_123@gmail.com";
+        boolean result ;
+        String email1="abcd_123@gmail.com";
+        String email2="qwe123ty@abcd.com";
+        String email3="xy%zacd@gmail.com";
+        list.add(email1);
+        list.add(email2);
+        list.add(email3);
+
         String regex="^[a-z0-9+_.-]+@+[a-z]+.+[a-z]";
         Pattern pattern=Pattern.compile(regex);
-        for(int i=0;i<email.length();i++){
-            Matcher matcher=pattern.matcher(email);
+        for(int i=0;i<list.size();i++){
+            Matcher matcher=pattern.matcher(list.get(i));
             result=matcher.matches();
+            System.out.println("The email "+list.get(i)+" is  =====>>>> "+result);
         }
-        System.out.println("The email is  =====>>>> "+result);
+        list.remove(email1);
+        list.remove(email2);
+        list.remove(email3);
     }
 
     private static void isValidPhoneNumber() {
@@ -54,14 +72,17 @@ public class UserRegistration {
         String phoneNumber="91 9734567892";
         String regex="[91]+() +[789][0-9]{9}";
         Pattern pattern=Pattern.compile(regex);
-        for(int i=0;i<phoneNumber.length();i++){
-            Matcher matcher=pattern.matcher(phoneNumber);
-            result=matcher.matches();
-        }
+        Matcher matcher=pattern.matcher(phoneNumber);
+        result=matcher.matches();
+        if(result==true)
+            list.add(phoneNumber);
+        else
+            list.add("-------------");
         System.out.println("The phone number is  =====>>>> "+result);
+
     }
     private static void isValidPassword() {
-        System.out.println("\n\n=========================================== UC5 ===============================================\n");
+        System.out.println("\n\n====================================== UC5, UC6, UC7 & UC8 =====================================\n");
         boolean result = false;
         String password="aAbcd@#1234";
 
@@ -70,10 +91,12 @@ public class UserRegistration {
 
         String regex="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
         Pattern pattern=Pattern.compile(regex);
-        for(int i=0;i<password.length();i++){
-            Matcher matcher=pattern.matcher(password);
-            result=matcher.matches();
-        }
+        Matcher matcher=pattern.matcher(password);
+        result=matcher.matches();
+        if(result==true)
+            list.add(password);
+        else
+            list.add("-------------");
         System.out.println("The password is  =====>>>> "+result);
     }
     }
