766a5b2d09c8512141a907cd9e56d463aa6fa5b9
AswathiMohan23 <achu999here@gmail.com>
2022-11-14 11:55:19 +0530

    [REFACTORED] : code to check whether first name is valid

--- src/com/java/UserRegistration.java
+++ src/com/java/UserRegistration.java
@@ -7,19 +7,28 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     static ArrayList<String> list=new ArrayList<>();
     public static void main(String[] args){
-        isValidName();
+        isValidFirstName();
     }
 
-    private static void isValidName() {
+    private static void isValidFirstName() {
         System.out.println("======================================= UC1 ===========================");
-        list.add("Harry");
+        String firstName="Harry";
+        boolean result = isNameValid(firstName);
+        System.out.println("\nthe first name is =====>>>>>  "+result);
+    }
+    private static boolean isNameValid(String name){
+        boolean result=false;
         String regex="[A-Z]{1}[a-z]{3,}";// {3,} means above 3
         Pattern pattern=Pattern.compile(regex);
-        for(int i=0;i<list.size();i++){
-            Matcher matcher=pattern.matcher((list.get(i)));
-            System.out.println("the first name is : ");
-            System.out.println(list.get(i)+ " -> "+matcher.matches());
+        for(int i=0;i<name.length();i++){
+            Matcher matcher=pattern.matcher(name);
+            result=matcher.matches();
+
         }
+
+
+
+        return result;
     }
 
 }
